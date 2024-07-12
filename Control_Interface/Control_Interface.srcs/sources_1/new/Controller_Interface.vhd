ef044349c889de87e94b566518bb8de6e624ad1e
janalo <janalo957@gmail.com>
2024-07-13 00:41:09 +0200

    set the outputs for FPGA

--- Control_Interface/Control_Interface.srcs/sources_1/new/Controller_Interface.vhd
+++ Control_Interface/Control_Interface.srcs/sources_1/new/Controller_Interface.vhd
@@ -19,7 +19,7 @@
 ----------------------------------------------------------------------------------
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
-USE IEEE.numeric_std.ALL; 
+USE IEEE.numeric_std.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -38,24 +38,24 @@ ENTITY Controller_Interface IS
         racket_height : INTEGER := 30;
         screen_height : INTEGER := 480
     );
-    PORT ( 
+    PORT (
         clock_i : IN STD_LOGIC;
-        reset_i : IN STD_LOGIC; 
-        rot_enc_i : IN STD_LOGIC_VECTOR (1 DOWNTO 0);  
+        reset_i : IN STD_LOGIC;
+        rot_enc_i : IN STD_LOGIC_VECTOR (1 DOWNTO 0);
         push_but_i : IN STD_LOGIC;
-        push_but_deb_o : OUT STD_LOGIC; 
-        racket_y_pos_o : OUT std_logic_vector (9 DOWNTO 0)
-        );
-END Controller_Interface; 
+        push_but_deb_o : OUT STD_LOGIC;
+        racket_y_pos_o : OUT STD_LOGIC_VECTOR (6 DOWNTO 0)
+    );
+END Controller_Interface;
 
 ARCHITECTURE Behavioral OF Controller_Interface IS
 
     SIGNAL debounceoutput_a_b : STD_LOGIC_VECTOR (1 DOWNTO 0);
-    Signal push_but_deb : STD_LOGIC := '0';
+    SIGNAL push_but_deb : STD_LOGIC := '0';
     SIGNAL pos_i : INTEGER := 209;
 
-    TYPE state_type IS (clockwise, counterclockwise);
-    SIGNAL current_state : state_type := clockwise; 
+    TYPE state_type IS (clockwise, counterclockwise, notrunning);
+    SIGNAL current_state : state_type := notrunning;
     SIGNAL next_state : state_type;
     SIGNAL a : STD_LOGIC := '0';
     SIGNAL b : STD_LOGIC := '0';
@@ -66,7 +66,7 @@ BEGIN
             GENERIC MAP(
                 clk_frequency_in_Hz => clk_frequency_in_Hz,
                 debounce_time_in_us => debounce_time_in_us,
-                active_low => true 
+                active_low => true
             )
             PORT MAP(
                 clk => clock_i,
@@ -75,26 +75,28 @@ BEGIN
                 debounce => debounceoutput_a_b(i)
             );
     END GENERATE;
-
-
     debounce_signal : ENTITY work.Rotation_Encoder_Debounced
-    GENERIC MAP(
-        clk_frequency_in_Hz => clk_frequency_in_Hz,
-        debounce_time_in_us => debounce_time_in_us,
-        active_low => true 
-    )
-    PORT MAP(
-        clk => clock_i,
-        rst => reset_i,
-        input => push_but_i,
-        debounce => push_but_deb
-    );
+        GENERIC MAP(
+            clk_frequency_in_Hz => clk_frequency_in_Hz,
+            debounce_time_in_us => debounce_time_in_us,
+            active_low => true
+        )
+        PORT MAP(
+            clk => clock_i,
+            rst => reset_i,
+            input => push_but_i,
+            debounce => push_but_deb
+        );
 
-    zuef_p : PROCESS (current_state, debounceoutput_a_b) 
+    zuef_p : PROCESS (current_state, debounceoutput_a_b)
 
     BEGIN
         a <= debounceoutput_a_b(0);
         b <= debounceoutput_a_b(1);
+
+        IF a = '0' AND b = '0' THEN
+            next_state <= notrunning;
+        END IF;
 
         IF rising_edge(a) THEN
             IF b = '0' THEN
@@ -112,34 +114,34 @@ BEGIN
     speicher_p : PROCESS (clock_i)
     BEGIN
         IF reset_i = '1' THEN
-            current_state <= clockwise;
+            current_state <= notrunning;
         ELSIF clock_i' event AND clock_i = '1' THEN
             current_state <= next_state;
         END IF;
     END PROCESS;
- 
+
     af_p : PROCESS (current_state)
     BEGIN
         CASE current_state IS
             WHEN clockwise =>
-                if pos_i > 449 then
-                     pos_i <= 449; 
-                 else 
-                     pos_i <= pos_i + racket_steps;
-                 end if;
+                IF pos_i > 449 THEN
+                    pos_i <= 449;
+                ELSE
+                    pos_i <= pos_i + racket_steps;
+                END IF;
             WHEN counterclockwise =>
-               if pos_i < 0 then
-                     pos_i <= 0;
-                 else 
-                     pos_i <= pos_i - racket_steps;
-                 end if;
+                IF pos_i < 0 THEN
+                    pos_i <= 0;
+                ELSE
+                    pos_i <= pos_i - racket_steps;
+                END IF;
             WHEN OTHERS =>
                 pos_i <= pos_i;
         END CASE;
- 
+
     END PROCESS;
 
-    racket_y_pos_o <= std_logic_vector(to_unsigned(pos_i, 10));  
-    push_but_deb_o <= push_but_deb;
+    racket_y_pos_o <= STD_LOGIC_VECTOR(to_unsigned(pos_i, 7));
+    push_but_deb_o <= not push_but_deb;
 
 END Behavioral;
