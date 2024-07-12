66f0797965cd352d37ed0a39712fad5117d2b810
Philipp Drüke <philipp.drueke@informatik.hs-fulda.de>
2024-07-13 00:48:53 +0200

    added score logic

--- Control_Interface/Control_Interface.srcs/sources_1/new/Score_Display.vhd
+++ Control_Interface/Control_Interface.srcs/sources_1/new/Score_Display.vhd
@@ -17,10 +17,8 @@
 -- Additional Comments:
 -- 
 ----------------------------------------------------------------------------------
-
-
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -31,22 +29,39 @@ use IEEE.STD_LOGIC_1164.ALL;
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
-entity Score_Display is
-    Generic( score_max : integer range 0 to 99 := 10); -- Max Point 
-    Port ( clock_i : in STD_LOGIC;
-           reset_i : in STD_LOGIC;
-           led_enable_i : in STD_LOGIC;
-           push_but1_deb_i : in STD_LOGIC;
-           push_but2_deb_i : in STD_LOGIC;
-           hit_wall_i : in STD_LOGIC_VECTOR (2 downto 0);
-           seven_seg_leds_o : out STD_LOGIC_VECTOR (6 downto 0);
-           seven_seg_sel_o : out STD_LOGIC_VECTOR (5 downto 0);
-           game_over_o : out STD_LOGIC);
-end Score_Display;
+ENTITY Score_Display IS
+    GENERIC (score_max : INTEGER RANGE 0 TO 99 := 10); -- Max Point 
+    PORT (
+        clock_i : IN STD_LOGIC;
+        reset_i : IN STD_LOGIC;
+        led_enable_i : IN STD_LOGIC;
+        push_but1_deb_i : IN STD_LOGIC;
+        push_but2_deb_i : IN STD_LOGIC;
+        hit_wall_i : IN STD_LOGIC_VECTOR (2 DOWNTO 0);
+        seven_seg_leds_o : OUT STD_LOGIC_VECTOR (6 DOWNTO 0);
+        seven_seg_sel_o : OUT STD_LOGIC_VECTOR (5 DOWNTO 0);
+        game_over_o : OUT STD_LOGIC);
+END Score_Display;
 
-architecture Behavioral of Score_Display is
+ARCHITECTURE Behavioral OF Score_Display IS
+    SIGNAL player1_score, player2_score : INTEGER RANGE 0 TO 99 := 0;
+    SIGNAL allow_restart : BOOLEAN := false;
 
-begin
-
-
-end Behavioral;
+BEGIN
+    PROCESS (hit_wall_i)
+    BEGIN
+        IF hit_wall_i = "101" THEN
+            IF player1_score = score_max THEN
+                allow_restart <= true;
+            ELSE
+                player1_score <= player1_score + 1;
+            END IF;
+        ELSIF hit_wall_i = "110" THEN
+            IF player2_score = score_max THEN
+                allow_restart <= true;
+            ELSE
+                player2_score <= player2_score + 1;
+            END IF;
+        END IF;
+    END PROCESS;
+END Behavioral;
