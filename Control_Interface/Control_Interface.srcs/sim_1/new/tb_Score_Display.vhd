eb52127ea7154dd5a479b7a93986975abcd9a0b2
janalo <janalo957@gmail.com>
2024-07-16 03:10:50 +0200

    testing done for score Display

---
+++ Control_Interface/Control_Interface.srcs/sim_1/new/tb_Score_Display.vhd
@@ -0,0 +1,117 @@
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+
+ENTITY tb_Score_Display IS
+END tb_Score_Display;
+
+ARCHITECTURE behavior OF tb_Score_Display IS
+
+    -- Component Declaration for the Unit Under Test (UUT)
+    COMPONENT Score_Display
+    GENERIC (score_max : INTEGER RANGE 0 TO 99 := 10);
+    PORT(
+         clock_i : IN STD_LOGIC;
+         reset_i : IN STD_LOGIC;
+         led_enable_i : IN STD_LOGIC;
+         push_but1_deb_i : IN STD_LOGIC;
+         push_but2_deb_i : IN STD_LOGIC;
+         hit_wall_i : IN STD_LOGIC_VECTOR (2 DOWNTO 0);
+         seven_seg_leds_o : OUT STD_LOGIC_VECTOR (6 DOWNTO 0);
+         seven_seg_sel_o : OUT STD_LOGIC_VECTOR (5 DOWNTO 0);
+         game_over_o : OUT STD_LOGIC
+        );
+    END COMPONENT;
+    
+    -- Inputs
+    SIGNAL clock_i : STD_LOGIC := '0';
+    SIGNAL reset_i : STD_LOGIC := '0';
+    SIGNAL led_enable_i : STD_LOGIC := '0';
+    SIGNAL push_but1_deb_i : STD_LOGIC := '0';
+    SIGNAL push_but2_deb_i : STD_LOGIC := '0';
+    SIGNAL hit_wall_i : STD_LOGIC_VECTOR (2 DOWNTO 0) := "000";
+
+    -- Outputs
+    SIGNAL seven_seg_leds_o : STD_LOGIC_VECTOR (6 DOWNTO 0);
+    SIGNAL seven_seg_sel_o : STD_LOGIC_VECTOR (5 DOWNTO 0);
+    SIGNAL game_over_o : STD_LOGIC;
+
+    -- Clock period definition
+    CONSTANT clock_period : TIME := 1 ms;
+
+BEGIN
+
+    -- Instantiate the Unit Under Test (UUT)
+    uut: Score_Display
+        GENERIC MAP (
+            score_max => 10
+        )
+        PORT MAP (
+            clock_i => clock_i,
+            reset_i => reset_i,
+            led_enable_i => led_enable_i,
+            push_but1_deb_i => push_but1_deb_i,
+            push_but2_deb_i => push_but2_deb_i,
+            hit_wall_i => hit_wall_i,
+            seven_seg_leds_o => seven_seg_leds_o, 
+            seven_seg_sel_o => seven_seg_sel_o,
+            game_over_o => game_over_o
+        );
+
+    -- Clock process definitions
+    clock_process : PROCESS
+    BEGIN
+        clock_i <= '0';
+        WAIT FOR  8ns;
+        clock_i <= '1';
+        WAIT FOR 8ns;
+    END PROCESS;
+
+    -- Stimulus process
+    stim_proc: PROCESS
+    BEGIN		
+        -- hold reset state for 100 ns.
+        reset_i <= '1';
+        WAIT FOR 100 ns;	
+        reset_i <= '0';
+        
+        -- Initial state: No button press, no hits
+        WAIT FOR 5 ms;
+        push_but1_deb_i <= '1';
+         push_but1_deb_i <= '1';
+        
+         WAIT FOR 5 ms;
+        
+ 
+        -- Continue simulation for player scores to reach the maximum score
+        FOR i IN 1 TO 12 LOOP
+            hit_wall_i <= "101";
+            WAIT FOR clock_period;
+            hit_wall_i <= "000";
+            WAIT FOR clock_period;
+            hit_wall_i <= "110";
+            WAIT FOR clock_period;
+        END LOOP;
+
+--        -- Now the left player should reach max score and game should be over
+--        hit_wall_i <= "101";
+--        WAIT FOR clock_period;
+--        hit_wall_i <= "000";
+--        WAIT FOR clock_period;
+        
+--        -- Check if game over is asserted
+--        ASSERT (game_over_o = '1')
+--        REPORT "Game over not asserted as expected" SEVERITY ERROR;
+        
+--        -- Simulate a reset button press to restart the game
+--        push_but1_deb_i <= '1';
+--        WAIT FOR clock_period;
+--        push_but1_deb_i <= '0';
+        
+--        -- Check if game has restarted
+--        ASSERT (game_over_o = '0')
+--        REPORT "Game did not restart as expected" SEVERITY ERROR;
+        
+        WAIT;
+    END PROCESS;
+
+END behavior;
--- Control_Interface/Control_Interface.srcs/sources_1/new/Score_Display.vhd
+++ Control_Interface/Control_Interface.srcs/sources_1/new/Score_Display.vhd
@@ -1,33 +1,5 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 10.07.2024 11:40:41
--- Design Name: 
--- Module Name: Score_Display - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 ENTITY Score_Display IS
     GENERIC (score_max : INTEGER RANGE 0 TO 99 := 10); -- Max Point 
@@ -37,7 +9,7 @@ ENTITY Score_Display IS
         led_enable_i : IN STD_LOGIC;
         push_but1_deb_i : IN STD_LOGIC;
         push_but2_deb_i : IN STD_LOGIC;
-        hit_wall_i : IN STD_LOGIC_VECTOR (2 DOWNTO 0);
+        hit_wall_i : IN STD_LOGIC_VECTOR (2 DOWNTO 0) := "000";
         seven_seg_leds_o : OUT STD_LOGIC_VECTOR (6 DOWNTO 0);
         seven_seg_sel_o : OUT STD_LOGIC_VECTOR (5 DOWNTO 0);
         game_over_o : OUT STD_LOGIC);
@@ -45,77 +17,70 @@ END Score_Display;
 
 ARCHITECTURE Behavioral OF Score_Display IS
     SIGNAL player_left_score, player_right_score : INTEGER RANGE 0 TO 99 := 0;
-    TYPE state_type IS (game_restarted,game_running, game_over);
-    SIGNAL current_state : state_type := game_running;
+    TYPE state_type IS (game_restarted, game_running, game_over);
+    SIGNAL current_state : state_type := game_restarted;
     SIGNAL next_state : state_type;
 BEGIN
 
---    restart : PROCESS (clock_i, push_but1_deb_i, push_but2_deb_i)
---    BEGIN
---        IF rising_edge(clock_i) THEN
-
---        END IF;
---    END PROCESS;
-    
-    zuef_p : PROCESS (clock_i, current_state, hit_wall_i, player_left_score, player_right_score)
+    zuef_p : PROCESS (hit_wall_i, current_state )
     BEGIN
-        if rising_edge(clock_i) THEN
-            CASE current_state is
-                when game_restarted => 
-                        player_left_score <= 0;
-                        player_right_score <= 0;
-                        next_state <= game_running;
-                when game_running =>
-                    if hit_wall_i = "101" THEN
-                        if(player_left_score = score_max) then
+            CASE current_state IS
+                WHEN game_restarted =>
+                    player_left_score <= 0;
+                    player_right_score <= 0;
+                    next_state <= game_running;
+
+                WHEN game_running =>
+                    IF hit_wall_i = "101" THEN
+                        IF (player_left_score = score_max) THEN
                             next_state <= game_over;
-                        else
+                        ELSE
                             player_left_score <= player_left_score + 1;
+                            next_state <= game_running;
                         END IF;
-                    elsif hit_wall_i = "101" THEN
-                        if(player_right_score = score_max) then
+                    ELSIF hit_wall_i = "110" THEN
+                        IF (player_right_score = score_max) THEN
                             next_state <= game_over;
-                        else
+                        ELSE
                             player_right_score <= player_right_score + 1;
-                        end if;    
+                            next_state <= game_running;
+                        END IF;
                     ELSE
                         next_state <= current_state;
                     END IF;
+
                 WHEN game_over =>
                     IF push_but1_deb_i = '1' OR push_but2_deb_i = '1' THEN
                         player_left_score <= 0;
                         player_right_score <= 0;
                         next_state <= game_running;
                     ELSE
-                    next_state <= current_state;
+                        next_state <= current_state;
                     END IF;
+
                 WHEN OTHERS =>
                     next_state <= current_state;
-                END CASE;
-            END IF;
+            END CASE;
     END PROCESS;
 
-    speicher_p : process(clock_i, reset_i)
-    begin
-        if reset_i = '1' then
+    speicher_p : PROCESS (clock_i, reset_i)
+    BEGIN
+        IF reset_i = '1' THEN
             current_state <= game_restarted;
         ELSIF rising_edge(clock_i) THEN
             current_state <= next_state;
-        else
-            current_state <= current_state;
-            next_state <= next_state;
         END IF;
     END PROCESS;
 
-    af_p : process(clock_i, current_state)
-    begin
+    af_p : PROCESS (clock_i, current_state)
+    BEGIN
         IF rising_edge(clock_i) THEN
-            if current_state = game_over then
+            IF current_state = game_over THEN
                 game_over_o <= '1';
-            else
+            ELSE
                 game_over_o <= '0';
             END IF;
         END IF;
-    end process;
+    END PROCESS;
 
 END Behavioral;
